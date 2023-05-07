05f996452d819ce4f9bf4cb7b9131d9a3eb655cc
GrassIsComplex <eneandresioan@gmail.com>
2023-05-07 20:54:42 +0300

    Main Menu

---
+++ scripts/menu_main_buttons.gd
@@ -0,0 +1,15 @@
+extends Control
+
+@export var play_scene : PackedScene
+@export var options_scene : PackedScene
+
+func _on_play_pressed():
+	pass # Replace with function body.
+
+
+func _on_options_pressed():
+	pass # Replace with function body.
+
+
+func _on_exit_pressed():
+	get_tree().root.exit
